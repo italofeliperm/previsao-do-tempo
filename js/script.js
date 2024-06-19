ce14252162927a52d343d3d61037fca5796a59aa
Italo <82714744+italofeliperm@users.noreply.github.com>
2024-06-19 12:33:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log('script carregado');
+
+const key = "94b08e79bbe84041102216fbb71e496c";
+
+function colocarDadosNaTela(dados){
+    console.log(dados);
+    document.querySelector(".cidade").innerHTML = "Weather in " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Air humidity: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric`).then(resposta => resposta.json());
+
+    colocarDadosNaTela(dados)
+} 
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+    buscarCidade(cidade);
+}
